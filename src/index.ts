81b298ac955bd7e8b8f42fe09f744e606da22d26
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-11-25 12:42:40 +0530

    feat: basic structure created

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,55 @@ import WebSocket, { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 });
 
-let allSockets = new Map();
+interface User {
+    socket: WebSocket;
+    room: string;
+}
+
+let allSockets: User[] = [];
 
 wss.on('connection', (socket) => {
+    wss.on('error', console.error);
+
     socket.on('message', (message) => {
-        socket.send(message.toString())
+        const parsedMessage = JSON.parse(message as unknown as string)
+
+        if (parsedMessage.type == 'create') {
+            allSockets.push({
+                socket,
+                room: parsedMessage.payload.roomId
+            })
+
+            socket.send('created successfully');
+        }
+
+        if (parsedMessage.type == 'join') {
+            allSockets.forEach(abc => {
+                if (abc.room == parsedMessage.payload.roomId) {
+                    allSockets.push({
+                        socket,
+                        room: parsedMessage.payload.roomId
+                    })
+
+                    return socket.send('joined successfully')
+                }
+            });
+        }
+
+        if (parsedMessage.type == "chat") {
+            let currentUserRoom = null;
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i].socket == socket) {
+                    currentUserRoom = allSockets[i].room
+                }
+            }
+
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i].room == currentUserRoom) {
+                    allSockets[i].socket.send(parsedMessage.payload.message)
+                }
+            }
+        }
     })
+
 })
